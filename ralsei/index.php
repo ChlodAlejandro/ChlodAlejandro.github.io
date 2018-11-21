53ac867303d53526eeaf0d03b2635efe74a03ef2
ChlodAlejandro <chlodaidanalejandro@hotmail.com>
2018-11-21 23:25:43 +0800

    forgot a few files

---
+++ ralsei/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ralsei Bot</title>
+		<style>
+		@font-face {
+			font-family: Determination Sans;
+			src: local("Determination Sans"),
+				url(DeterminationSansWeb.woff);
+		}
+		body {
+			font-family: "Determination Sans", sans-serif;
+			background-color: #23272A;
+			color: white;
+			text-align: center;
+				font-size: 2em;
+				padding: 0 25vw;
+		}
+		h1 {
+			color: #7289DA;
+		}
+		h4 {
+			font-size: 0.5em;
+			font-weight: normal;
+		}
+		a, a:link, a:visited, a:active, a:focus, a:hover {
+			text-decoration: none;
+			color: #7289DA;
+		}
+		table {
+			width: 100%;
+			font-size: 0.5em;
+		}
+		img {
+			width: 80%;
+			height: auto;
+		}
+		img.shrinked {
+		width: 50%;
+		height: auto;
+		}
+		img.vshrinked {
+		width: 25%;
+		height: auto;
+		}
+		</style>
+	</head>
+	<body>
+		<h1>RALSEI BOT</h1>
+		<p>Because this fluffy boy needs to live on forever</p>
+		<p>You can get this bot on your Discord server by clicking <a href="https://discordapp.com/api/oauth2/authorize?client_id=513344063106187270&permissions=51200&scope=bot">here</a>.</p>
+		<h4 class="small">If the bot goes offline due to inactivity, <s>just visit this website again to restart it</s> (the website and bot now work as seperate units to conserve important runtime hours. This will no longer work, but the bot will keep itself awake as much as possible. If it stays offline, contact the developer immediately.)</h4>
+		<img class="shrinked" src="https://discordapp.com/assets/e7a3b51fdac2aa5ec71975d257d5c405.png">
+		<p>Ralsei can provide varied responses for an action. An example of those can be found below (only one is shown.)</p>
+		<table border=1>
+			<tr>
+				<th>Command</th>
+				<th>Action</th>
+			</tr>
+			<tr>
+				<td>!ralsei OR "hi ralsei" and similar</td>
+				<td><img src="images/ralsei-hello.png"></td>
+			</tr>
+			<tr>
+				<td>!ralsei bye OR "bye ralsei" and similar</td>
+				<td><img src="images/ralsei-bye.png"></td>
+			</tr>
+			<tr>
+				<td>"*hugs ralsei*"</td>
+				<td><img src="images/ralsei-hugreply.png"></td>
+			</tr>
+			<tr>
+				<td>!ralsei hug &lt;user&gt; OR "hug me ralsei"</td>
+				<td><img src="images/ralsei-hug.png"></td>
+			</tr>
+			<tr>
+				<td>"i love you ralsei" or similar</td>
+				<td><img src="images/ralsei-love.png"></td>
+			</tr>
+			<tr>
+				<td>!ralsei help OR !ralsei commands</td>
+				<td>Displays the commands. Soon to look like Ralsei's manual from Deltarune.</td>
+			</tr>
+		</table>
+		<p>Todo: Ralsei-fied swear filter. And more Ralsei-ness. Suggest me features too. Subscribe to the <a href="https://reddit.com/r/ralsei">Ralsei subreddit</a>.</p>
+		<h4 class="small">If you have questions, please contact @ChlodAlejandro on Twitter</h4>
+		<h4 class="small">Note: You do not get the testing version of Ralsei. You get the full packaged Ralsei.</h4>
+		<h4 class="small">Uses <a href="https://demirramon.com/en/generators/undertale_text_box_generator">Demirramon's Undertale Text Box Generator</a>.</h4>
+		<img class="shrinked" src="images/screenshot.png">
+	</body>
+</html>
